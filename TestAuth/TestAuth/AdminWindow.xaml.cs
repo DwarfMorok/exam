c8cfb4571761c83f48a93be3394d9610d8b66b20
Aleks Mor <162573539+DwarfMorok@users.noreply.github.com>
2025-12-25 22:11:13 +0300

    Add files via upload

---
+++ TestAuth/TestAuth/AdminWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MedicalLab
+{
+    public partial class AdminWindow : Window
+    {
+        SqlConnection conn;
+
+        public AdminWindow(SqlConnection connection)
+        {
+            InitializeComponent();
+            conn = connection;
+            RefreshEmployees();
+            RefreshOrders();
+        }
+
+        void RefreshEmployees(object sender = null, RoutedEventArgs e = null)
+        {
+            var cmd = new SqlCommand(
+                @"SELECT e.id, e.full_name, e.login, er.role_name, 
+                  e.login_attempts, e.is_blocked
+                  FROM employees e
+                  JOIN employee_roles er ON e.role_id = er.id",
+                conn);
+
+            var table = new System.Data.DataTable();
+            new SqlDataAdapter(cmd).Fill(table);
+            gridEmployees.ItemsSource = table.DefaultView;
+        }
+
+        void RefreshOrders(object sender = null, RoutedEventArgs e = null)
+        {
+            var cmd = new SqlCommand(
+                @"SELECT o.order_number, p.full_name, o.order_date, o.total_amount
+                  FROM orders o
+                  JOIN patients p ON o.patient_id = p.id",
+                conn);
+
+            var table = new System.Data.DataTable();
+            new SqlDataAdapter(cmd).Fill(table);
+            gridOrders.ItemsSource = table.DefaultView;
+        }
+    }
+}
---
+++ TestAuth/TestAuth/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.SqlClient;
+using System.Windows;
+
+namespace MedicalLab
+{
+    public partial class MainWindow : Window
+    {
+        SqlConnection conn;
+
+        public MainWindow()
+        {
+            InitializeComponent(); // Это должно быть первым!
+
+            try
+            {
+                conn = new SqlConnection(@"Server=localhost\SQLEXPRESS;Database=MedicalLab;Trusted_Connection=True;");
+                conn.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Не могу подключиться к БД!");
+            }
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string login = txtLogin.Text;
+            string pass = txtPassword.Password;
+
+            var cmd = new SqlCommand(
+                "SELECT * FROM employees WHERE login=@l AND password_hash=@p",
+                conn);
+            cmd.Parameters.AddWithValue("@l", login);
+            cmd.Parameters.AddWithValue("@p", pass);
+
+            var r = cmd.ExecuteReader();
+            if (r.Read())
+            {
+                MessageBox.Show("Успешный вход!");
+                new AdminWindow(conn).Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Ошибка входа!");
+            }
+            r.Close();
+        }
+    }
+}
